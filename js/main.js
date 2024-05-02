a8ec2388b2144a17fd4f9cea0003e0c288a775f5
Sneatx <santi02005@hotmail.com>
2024-05-02 17:37:35 -0500

    feat: :construction: En proceso del cambio de logica a uso de querys

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ import { mostrarBarras } from "./modules/mostrarBarras.js"
 
 let selectRocket = document.getElementById("selectRocket")
 selectRocket.addEventListener("change" , async(event) =>{
-    selectRocket.value === '' ? document.querySelector(".progressBars__allItems").innerHTML = '' : await mostrarBarras(Number(selectRocket.value))
+    selectRocket.value === '' ? document.querySelector(".progressBars__allItems").innerHTML = '' : await mostrarBarras(selectRocket.value)
 })
--- js/modules/mostrarBarras.js
+++ js/modules/mostrarBarras.js
@@ -1,34 +1,17 @@
-export const mostrarBarras = async(value) =>{
-    let data = await(await fetch("https://api.spacexdata.com/v4/rockets")).json()
-
-    let cohete = data[value]
-
-    let alturas = []
-    let diametros = []
-    let masas = []
-    let costsPerLaunchs = []
-    let successRate = []
-
-    for(let rocket of data){
-        alturas.push(rocket.height.meters)
-        diametros.push(rocket.diameter.meters)
-        masas.push(rocket.mass.kg)
-        costsPerLaunchs.push(rocket.cost_per_launch)
-        successRate.push(rocket.success_rate_pct)
-    }
+export const mostrarBarras = async(id) =>{
+    let cohete = await(await fetch(`https://api.spacexdata.com/v4/rockets/${id}`)).json()
     
-    let alturaMax = hallarMaximo(alturas)
-    let diametroMax = hallarMaximo(diametros)
-    let masaMax = hallarMaximo(masas)
-    let costoMaximo = hallarMaximo(costsPerLaunchs)
-    let exitoMaximo = hallarMaximo(successRate)
+    let heightMax = await getRocketHeightMaxInM()
+    let diameterMax = await getRocketDiameterMaxInM()
+    let massMax = await getRocketMassMaxInKg()
+    let costPerLaunchMax
 
     document.querySelector(".progressBars__allItems").innerHTML = ""
-    insertarBarra(cohete.height.meters, alturaMax, "Height", "m")
-    insertarBarra(cohete.diameter.meters, diametroMax, "Diameter", "m")
-    insertarBarra(cohete.mass.kg, masaMax, "Mass", "kg")
-    insertarBarra(cohete.cost_per_launch, costoMaximo, "Cost per launch", "$")
-    insertarBarra(cohete.success_rate_pct, exitoMaximo, "Success rate", "%")
+    insertarBarra(cohete.height.meters, heightMax, "Height", "M")
+    insertarBarra(cohete.diameter.meters, diameterMax, "Diameter", "M")
+    insertarBarra(cohete.mass.kg, massMax, "Mass", "kg")
+
+
 }
 
 function insertarBarra(valor, valorMax, titulo, unidad){
@@ -63,62 +46,74 @@ function insertarBarra(valor, valorMax, titulo, unidad){
     document.querySelector(".progressBars__allItems").appendChild(divContenedor)
 }
 
-// function insertarBarras (rocket, alturaMax, diametroMax, masaMax){
-
-//     let porcentajeAltura = (rocket.height.meters / alturaMax) * 100
-//     let porcentajeDiametro = (rocket.diameter.meters / diametroMax) * 100
-//     let porcentajeMasa = (rocket.mass.kg / masaMax) * 100
-
-//     document.querySelector(".progressBars__allItems").innerHTML = `
-//     <section class="progressBars__item">
-//         <div class="progressBars__item__title">
-//             <h1>Height</h1>
-//         </div>
-//         <div class="progressBars__item__info">
-//             <div class="info__bar">
-//                 <div class="bar" style="width: ${porcentajeAltura}%;"></div>
-//             </div>
-//             <div class="info__text">
-//                 <p>${rocket.height.meters} m</p>
-//             </div>
-//         </div>
-//     </section>
-
-//     <section class="progressBars__item">
-//         <div class="progressBars__item__title">
-//             <h1>Diameter</h1>
-//         </div>
-//         <div class="progressBars__item__info">
-//             <div class="info__bar">
-//                 <div class="bar" style="width: ${porcentajeDiametro}%;"></div>
-//             </div>
-//             <div class="info__text">
-//                 <p>${rocket.diameter.meters} m</p>
-//             </div>
-//         </div>
-//     </section>
-
-//     <section class="progressBars__item">
-//     <div class="progressBars__item__title">
-//         <h1>Mass</h1>
-//     </div>
-//     <div class="progressBars__item__info">
-//         <div class="info__bar">
-//             <div class="bar" style="width: ${porcentajeMasa}%;"></div>
-//         </div>
-//         <div class="info__text">
-//             <p>${rocket.mass.kg} Kg</p>
-//         </div>
-//     </div>
-// </section>
-// `
-// }
-
-function hallarMaximo (arr){
-    let max = arr[0]
-
-    for(let i = 1; i < arr.length; i++){
-        arr[i] > max ? max = arr[i] : arr[i]
+export const getRocketMassMaxInKg  = async() =>{
+    let config = {
+        headers:{
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "options": {
+                "select": {
+                    "mass": 1
+                },
+                "sort":{
+                    "mass.kg": "desc"
+                },
+                "limit": 1
+            }
+        })
     }
-    return max
+    let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
+    let {docs:[{mass:{kg}} = maxMassRocket]} = await res.json();
+    return kg;
 }
+
+export const getRocketHeightMaxInM = async() =>{
+    let config = {
+        headers:{
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "options": {
+                "select":{
+                    "height": 1
+                },
+                "sort":{
+                    "height.meters" : "desc"
+                },
+                "limit" : 1
+            },
+
+        })
+    }
+    let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
+    let {docs:[{height:{meters}} = maxHeightRocket]} = await res.json();
+    return meters; 
+}
+
+export const getRocketDiameterMaxInM = async() =>{
+    let config = {
+        headers:{
+            "content-type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "options": {
+                "select":{
+                    "diameter": 1
+                },
+                "sort":{
+                    "diameter.meters" : "desc"
+                },
+                "limit" : 1
+            },
+
+        })
+    }
+    let res = await fetch("https://api.spacexdata.com/v4/rockets/query", config);
+    let {docs:[{diameter:{meters}} = maxDiameterRocket]} = await res.json();
+    return meters; 
+}
+
