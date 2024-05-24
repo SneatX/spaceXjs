8c10a4b2e9082796be2082873c660e223738a627
SneatX <santi02005@hotmail.com>
2024-05-24 14:15:26 -0500

    feat: :bug: Opcion predefinida en ambas apis

--- js/modules/rockets/ui/opcionesCohete.js
+++ js/modules/rockets/ui/opcionesCohete.js
@@ -2,7 +2,7 @@ import {getAllRockets} from "../services/apiService.js"
 
 export const mostrarOpcionesCohete = async() =>{
     let data = await getAllRockets()
-
+    document.getElementById("selectRocket").innerHTML = `<option value="">Select</option>`
     data.forEach((cohete, indice) => {
         let strHTML = `<option value="${cohete.id}">Rocket ${indice+1}</option>`
         document.getElementById("selectRocket").innerHTML += strHTML
--- js/modules/starlink/ui/opcionesStarlink.js
+++ js/modules/starlink/ui/opcionesStarlink.js
@@ -2,7 +2,7 @@ import {getAllStarlink} from "../services/apiService.js"
 
 export const mostrarOpcionesStarlink = async() =>{
     let data = await getAllStarlink()
-    document.getElementById("selectStarlink").innerHTML = ""
+    document.getElementById("selectStarlink").innerHTML = `<option value="">Select</option>`
     for(let i = 10; i < 30; i++) {
         let satelite = data[i]
         let strHTML = `<option value="${satelite.id}">Satelite ${i+1}</option>`
